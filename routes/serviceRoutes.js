a0ba129ee9cac86d1edd2be5542e37b8be196314
IWR2 <65472832+IWR2@users.noreply.github.com>
2026-06-05 21:18:29 -0700

    Created post and get routes for creating a service and getting as service.

---
+++ routes/serviceRoutes.js
@@ -0,0 +1,37 @@
+/**
+ * Service Routes - Handles all /services endpoint operations.
+ *
+ * @module routes/servicesRoutes
+ */
+
+import express from "express";
+import {
+  createService,
+  getAService,
+} from "../controllers/serviceController.js";
+
+const router = express.Router();
+
+// Service CRUD operations
+router.post("/", createService);
+
+router.get("/:id", getAService);
+//TODO: router.get("/", getAllServices);
+
+// Method Not Allowed handlers (for root path only)
+router.put("/", (req, res) => {
+  res.set("Accept", "PUT");
+  res.status(405).json({ Error: "Method not allowed" });
+});
+
+router.patch("/", (req, res) => {
+  res.set("Accept", "PATCH");
+  res.status(405).json({ Error: "Method not allowed" });
+});
+
+router.delete("/", (req, res) => {
+  res.set("Accept", "DELETE");
+  res.status(405).json({ Error: "Method not allowed" });
+});
+
+export default router;
